3cb390abfac833f9b29432086b42ebbd458fda53
Tenzin <tenzin.pepper@student.curtin.edu.au>
2026-03-23 22:07:59 +0800

    Add home and calendar view with navigation and styles

--- script.js
+++ script.js
@@ -22,4 +22,33 @@ function updateCalendar() {
   document.getElementById("month").textContent = month;
 }
 
+function showView(viewId) {
+  const views = ["homeView", "calendarView"];
+
+  views.forEach((id) => {
+    const view = document.getElementById(id);
+    if (!view) {
+      return;
+    }
+
+    view.classList.toggle("hidden-view", id !== viewId);
+  });
+}
+
+const openCalendarBtn = document.getElementById("openCalendarBtn");
+const backToHomeBtn = document.getElementById("backToHomeBtn");
+
+if (openCalendarBtn) {
+  openCalendarBtn.addEventListener("click", () => {
+    showView("calendarView");
+  });
+}
+
+if (backToHomeBtn) {
+  backToHomeBtn.addEventListener("click", () => {
+    showView("homeView");
+  });
+}
+
+showView("homeView");
 updateCalendar();
